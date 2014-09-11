4562969f3c3b75abac74c45b9cdafb854980ebac
Wei Gao <jky239@gmail.com>
2014-09-11 16:14:14 +0800

    start restify server in sync command

--- bin/sync.js
+++ bin/sync.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
 
 var sync = require('../lib').sync;
+var restify = require('../lib').restify;
 
 sync();
+
+var adminPort = 4433;
+restify.listen(adminPort, function() {
+  console.log('%s listening at %s', restify.name, adminPort);
+});
